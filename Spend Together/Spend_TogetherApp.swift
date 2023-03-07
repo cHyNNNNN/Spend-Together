5b5cc039524f83d8872fef6bf1bb30560176fba6
José Manuel Roldán Gómez <jmrgranada@gmail.com>
2023-03-07 15:24:48 +0200

    Initial Commit

---
+++ Spend Together/Spend_TogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_TogetherApp.swift
+//  Spend Together
+//
+//  Created by Jose Manuel Roldan Gomez on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Spend_TogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
